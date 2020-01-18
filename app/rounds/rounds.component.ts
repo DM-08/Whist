c8c8b3e3a2d4753d23b4cf64b93a2675331e8140
DM-08 <56652167+DM-08@users.noreply.github.com>
2020-01-18 21:55:52 +0000

    rounds file changes

--- app/rounds/rounds.component.ts
+++ app/rounds/rounds.component.ts
@@ -8,19 +8,23 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   //styleUrls: ['./rounds.component.css']
 })
 export class RoundsComponent implements OnInit {
-//  @Input() rd :Round;
-//  @Output() onChange = new EventEmitter();
+  //@Input() rd :Round;
+  //@Output() onChange = new EventEmitter();
   roundNumber: number;
-  rounds: string[];
+  //Array of Rounds
+  rounds: Round[]=[];
   scores=[0,1,2,3,4,5,6,7,8,9,10,11,12,13];
   suit=["No Trump","Hearts","Spades","Clubes","Diamonds"];
   proceed:boolean;
-  round ={name:'',trump: this.suit[0], score: this.scores[0]};
+  round:Round;
   scr :number;
   selected: string;
   roundForm : FormGroup;
 
-  constructor() { }
+
+  constructor() {
+    this.round={name:'',trump: this.suit[0], score: this.scores[0]};
+  }
 
   ngOnInit() {
     this.roundNumber = 1;
@@ -37,23 +41,46 @@ export class RoundsComponent implements OnInit {
   get name() { return this.roundForm.get('name'); }
 
   Forward(): void {
-    //this.proceed=true;
     this.proceed=false;
-    if (this.roundNumber < 10) {
-     //rounds[round-1]=this.partner_name;
-     this.roundNumber += 1;
-    }
-    console.log(this.roundForm.get('name').value+"   "+
+    /*console.log(this.roundForm.get('name').value+"   "+
     this.roundForm.get('trump').value+"  "+
     this.roundForm.get('score').value);
-    this.scr=0;
-    this.scr=this.roundForm.get('score').value;
-    console.log(this.scr);
+    */
+    //round ={name:'',trump: this.suit[0], score: this.scores[0]};
+    this.round.name=this.roundForm.get('name').value;
+    this.round.trump=this.roundForm.get('trump').value;
+    this.round.score=this.roundForm.get('score').value;
+    console.log(this.round.name+"  "+this.round.score+"  "+this.round.trump);
+    console.log(this.roundNumber+" rn");
+    this.rounds[this.roundNumber-1] = this.round;
+    console.log(this.rounds[this.roundNumber-1].name+" name from array");
+
+    if (this.roundNumber < 10) {
+      console.log(this.rounds.length+" array size 1st");
+      if(this.rounds.length>this.roundNumber){
+        //set the already saved values once again
+        console.log("in if "+this.round.name);
+        this.roundForm.controls['name'].setValue(this.rounds[this.roundNumber].name);
+        this.roundForm.controls['trump'].setValue(this.rounds[this.roundNumber].trump);
+        this.roundForm.controls['score'].setValue(this.rounds[this.roundNumber].score);
+      }
+      else{
+        this.roundForm.controls['name'].reset();
+      }
+      this.roundNumber += 1;
+      this.round ={name:'',trump: this.suit[0], score: this.scores[0]};
+    }
   }
 
   Back(){
+    console.log(this.rounds.length+" array size");
+    console.log("in back rd: "+this.roundNumber);
     if (this.roundNumber >1) {
      this.roundNumber -= 1;
+     this.roundForm.controls['name'].setValue(this.rounds[this.roundNumber-1].name);
+     this.roundForm.controls['trump'].setValue(this.rounds[this.roundNumber-1].trump);
+     this.roundForm.controls['score'].setValue(this.rounds[this.roundNumber-1].score);
+
     }
     this.proceed=true;
   }
